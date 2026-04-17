8e4c8a86467d95ec3697ce0ca790f7fa594004a4
JustAnother-cdm <johan.restrepo7197@gmail.com>
2026-04-17 15:46:26 -0500

    Merge

--- src/main/java/controllers/inicioController.java
+++ src/main/java/controllers/inicioController.java
@@ -1,4 +1,26 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class InicioController {
+
+    @FXML
+    private PasswordField Contraseña;
+
+    @FXML
+    private TextField correo;
+
+    @FXML
+    void logIn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUp(ActionEvent event) {
+
+    }
+
 }
