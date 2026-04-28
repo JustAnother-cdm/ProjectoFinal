732b294e78de536d676656e4e697e70740031367
JustAnother-cdm <johan.restrepo7197@gmail.com>
2026-04-28 14:06:00 -0500

    se añadio la clase Personana.java para la herencia

--- src/main/java/controllers/inicioController.java
+++ src/main/java/controllers/inicioController.java
@@ -1,27 +1,4 @@
 package controllers;
 
-
-import application.Main;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import utilities.Paths;
-
 public class InicioController {
-    @FXML
-    private PasswordField Contraseña;
-
-    @FXML
-    private TextField id;
-
-    @FXML
-    void logIn(ActionEvent event) {
-
-    }
-
-    @FXML
-    void signUp(ActionEvent event) {Main.main.setScene(Paths.INICIO);}
-
 }
-
